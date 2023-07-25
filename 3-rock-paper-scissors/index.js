4ef2f8d6769648f8147d1d45b4adbffb2963d281
Bruno Vidal <brunokasprik@gmail.com>
2023-07-24 23:16:17 -0300

    project 3

---
+++ 3-rock-paper-scissors/index.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
