3d0e2c2b14accb26896f7c0584652345bbce90aa
Bruno Vidal <brunokasprik@gmail.com>
2023-08-03 01:16:53 -0300

    added rgb mode and finished layout

--- 4-etch-a-sketch/index.js
+++ 4-etch-a-sketch/index.js
@@ -1,52 +1,64 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const sketchBox = document.querySelector(".sketch-box");
-  const resetButton = document.getElementById("resetButton");
+document.addEventListener('DOMContentLoaded', function () {
+  let rgbmode = false
+  const sketchBox = document.querySelector('.sketch-box')
+  const gridSizeSlider = document.getElementById('gridSizeSlider')
+  const resetButton = document.getElementById('resetButton')
+  const rgbButton = document.getElementById('rgbButton')
 
-  
   function createSquare() {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", changeColor);
-    return square;
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.addEventListener('mouseover', changeColor)
+    return square
+  }
+  function getRandomRGBColor() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
   }
 
-  
   function changeColor(e) {
-    e.target.style.backgroundColor = "black";
+    if (!rgbmode) {
+      e.target.style.backgroundColor = 'black'
+    } else {
+      e.target.style.backgroundColor = getRandomRGBColor()
+    }
   }
 
-  
   function clearGrid() {
-    sketchBox.innerHTML = "";
+    sketchBox.innerHTML = ''
+  }
+  function rgbModeSwitch() {
+    rgbmode = !rgbmode
   }
 
-  
   function createNewGrid() {
-    let gridSize = parseInt(prompt("Enter the number of squares per side (max 100):"));
-    gridSize = isNaN(gridSize) ? 16 : Math.min(gridSize, 100);
-    const squareSize = (550 - (gridSize - 1) * 2) / gridSize; 
+    const gridSize = gridSizeSlider.value
+    document.querySelector(
+      '.sizetext'
+    ).innerText = `Grid Size: ${gridSize} x ${gridSize}`
+    const squareSize = (550 - (gridSize - 1) * 2) / gridSize
 
-    clearGrid();
+    clearGrid()
 
-    sketchBox.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-    sketchBox.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    sketchBox.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    sketchBox.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
 
     for (let i = 0; i < gridSize; i++) {
       for (let j = 0; j < gridSize; j++) {
-        const square = createSquare();
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        sketchBox.appendChild(square);
+        const square = createSquare()
+        square.style.width = `${squareSize}px`
+        square.style.height = `${squareSize}px`
+        sketchBox.appendChild(square)
       }
     }
   }
 
-  
-  createNewGrid();
+  createNewGrid()
 
+  gridSizeSlider.addEventListener('input', createNewGrid)
 
-  resetButton.addEventListener("click", createNewGrid);
-});
-
-
-
+  resetButton.addEventListener('click', createNewGrid)
+  rgbButton.addEventListener('click', rgbModeSwitch)
+})
