59f58d2120b453dd56e488e25281e977d0112b53
Bruno Vidal <brunokasprik@gmail.com>
2023-08-07 19:03:38 -0300

    added js function to match passwords

--- 6-sign-up-form/index.js
+++ 6-sign-up-form/index.js
@@ -1,1 +1,39 @@
-//do password check
+const passwordInput = document.getElementById('password')
+const confirmPasswordInput = document.getElementById('confirmPassword')
+const errorText = document.querySelector('.password-match')
+const minChars = document.querySelector('.min-char')
+const registerButton = document.querySelector('button')
+
+function checkPasswords() {
+  const password = passwordInput.value
+  const confirmPassword = confirmPasswordInput.value
+  console.log(password.length)
+  if (password.length < 8) {
+    minChars.style.display = 'block'
+    passwordInput.style.borderColor = 'red'
+    return
+  } else {
+    passwordInput.style.borderColor = 'green'
+    minChars.style.display = 'none'
+  }
+
+  if (!confirmPassword) {
+    errorText.style.display = 'none'
+    return
+  }
+
+  if (password === confirmPassword) {
+    registerButton.removeAttribute('disabled')
+    confirmPasswordInput.style.borderColor = 'green'
+    errorText.style.display = 'none'
+    console.log('Passwords match.')
+  } else {
+    confirmPasswordInput.style.borderColor = 'red'
+    errorText.style.display = 'block'
+    registerButton.setAttribute('disabled', 'true')
+    console.log('Passwords do not match.')
+  }
+}
+
+passwordInput.addEventListener('input', checkPasswords)
+confirmPasswordInput.addEventListener('input', checkPasswords)
