6283d2cc43c18353b118638542fcc551fce7f19c
Bruno Vidal <brunokasprik@gmail.com>
2023-08-31 00:35:14 -0300

    add new book modal done

--- 8-library/index.js
+++ 8-library/index.js
@@ -1,2 +1,9 @@
 const searchInputContainer = document.querySelector('.search-bar');
+const createBookButton = document.querySelector('.create-book-btn')
+const showBookModal = document.querySelector('.book-modal')
+const hideBookModal = document.querySelector('#closeBtn')
+
+
+createBookButton.onclick = () => showBookModal.showModal()
+hideBookModal.onclick = () => showBookModal.close()
 
