cab8e31dd0ad26684508bcf5649cb14389e56436
Bruno Vidal <brunokasprik@gmail.com>
2023-09-02 03:54:15 -0300

    choose mode interface done

--- 9-tic-tac-toe/app.js
+++ 9-tic-tac-toe/app.js
@@ -1,10 +1,16 @@
-const cellElements = document.querySelectorAll('[data-cell]');
-const board = document.getElementById('board');
-const winningModal = document.querySelector('.winning-message-modal');
-const winningMsg = document.querySelector('.winning-message');
-const restartBtn = document.getElementById('restart-button');
-const X_CLASS = 'x';
-const CIRCLE_CLASS = 'circle';
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningModal = document.querySelector('.winning-message-modal')
+const winningMsg = document.querySelector('.winning-message')
+const restartBtn = document.getElementById('restart-button')
+const menuModal = document.querySelector('.menu-modal')
+const pvpElement = document.querySelector('.pvp')
+const pveElement = document.querySelector('.pve')
+let mode
+let currentPlayer = X_CLASS
+
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -14,60 +20,71 @@ const WINNING_COMBINATIONS = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
-];
+]
 
-startGame();
+menu()
 
-restartBtn.addEventListener('click', startGame);
+winningModal.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    event.preventDefault()
+  }
+})
+
+restartBtn.addEventListener('click', startGame)
 
 function handleClick(e) {
-  const cell = e.target;
-  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-  placeMark(cell, currentClass);
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
 
   if (checkWin(currentClass)) {
-    endGame(false);
+    endGame(false)
   } else if (isDraw()) {
-    endGame(true);
+    endGame(true)
   } else {
-    swapTurns();
-    setBoardHoverClass();
+    swapTurns()
+    setBoardHoverClass()
   }
 }
 
+function menu() {
+  menuModal.showModal()
+}
+
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass);
+  cell.classList.add(currentClass)
 }
 
 function endGame(draw) {
-  winningMsg.innerText = draw ? 'Draw!' : `${circleTurn ? 'O' : 'X'} Wins!`;
-  winningModal.showModal();
+  winningMsg.innerText = draw ? 'Draw!' : `${circleTurn ? 'O' : 'X'} Wins!`
+  winningModal.showModal()
 }
 
 function isDraw() {
-  return [...cellElements].every((cell) =>
-    cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-  );
+  return [...cellElements].every(
+    (cell) =>
+      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  )
 }
 
 function swapTurns() {
-  circleTurn = !circleTurn;
+  circleTurn = !circleTurn
 }
 
 function setBoardHoverClass() {
-  board.classList.remove(X_CLASS, CIRCLE_CLASS);
-  board.classList.add(circleTurn ? CIRCLE_CLASS : X_CLASS);
+  board.classList.remove(X_CLASS, CIRCLE_CLASS)
+  board.classList.add(circleTurn ? CIRCLE_CLASS : X_CLASS)
 }
 
 function startGame() {
-  circleTurn = false;
+  circleTurn = false
   cellElements.forEach((cell) => {
-    cell.classList.remove(X_CLASS, CIRCLE_CLASS);
-    cell.removeEventListener('click', handleClick);
-    cell.addEventListener('click', handleClick, { once: true });
-  });
-  setBoardHoverClass();
-  winningModal.close();
+    cell.classList.remove(X_CLASS, CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningModal.close()
 }
 
 function checkWin(currentClass) {
@@ -75,5 +92,18 @@ function checkWin(currentClass) {
     combination.every((index) =>
       cellElements[index].classList.contains(currentClass)
     )
-  );
+  )
 }
+
+pvpElement.addEventListener('click', () => {
+  mode = 'pvp'
+  // You can add your logic here
+  console.log('PvP mode was clicked')
+})
+
+pveElement.addEventListener('click', () => {
+  mode = 'pve'
+  // Handle PvE mode click
+  // You can add your logic here
+  console.log('PvE mode was clicked')
+})
