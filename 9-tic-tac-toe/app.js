ce5b041fb19cb5913f19da044c3b55b2d9ed9b8e
Bruno Vidal <brunokasprik@gmail.com>
2023-09-01 23:51:28 -0300

    css board done

---
+++ 9-tic-tac-toe/app.js
@@ -0,0 +1,70 @@
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+let circleTurn
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+startGame()
+
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  }
+  swapTurns()
+  setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function endGame(draw) {
+  if (draw) {
+    
+  } else {
+    
+  }
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach((cell) => {
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
