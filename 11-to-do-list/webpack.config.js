867d0363e3d0f0d5d93874b67672c8484bd9123f
Bruno Vidal <brunokasprik@gmail.com>
2023-09-06 17:09:04 -0300

    todo list 1st commit

---
+++ 11-to-do-list/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+};
