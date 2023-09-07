1af5615288be42cf79513e6c88a136257a923ef7
Bruno Vidal <brunokasprik@gmail.com>
2023-09-07 01:33:57 -0300

    new todo form html/css done

--- 11-to-do-list/main.js
+++ 11-to-do-list/main.js
@@ -1,3 +1,10 @@
 import './style.css'
+const newTodoModal = document.querySelector('.new-todo-modal')
+const addTodoBtn = document.querySelector('.plus-todo')
 
+
+addTodoBtn.addEventListener('click', () => {
+  newTodoModal.showModal()
+  
+})
 
