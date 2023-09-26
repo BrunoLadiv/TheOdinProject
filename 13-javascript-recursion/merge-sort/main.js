68c010c23900de5c0557f808b3508d2f046a0a4c
Bruno Vidal <brunokasprik@gmail.com>
2023-09-26 02:43:49 -0300

    merge-sort

---
+++ 13-javascript-recursion/merge-sort/main.js
@@ -0,0 +1,37 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr
+  }
+
+  const middle = Math.floor(arr.length / 2)
+
+  const leftHalf = arr.slice(0, middle)
+  const rightHalf = arr.slice(middle)
+
+  const sortedLeft = mergeSort(leftHalf)
+  const sortedRight = mergeSort(rightHalf)
+
+  return merge(sortedLeft, sortedRight)
+}
+
+function merge(left, right) {
+  let result = []
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex])
+      leftIndex++
+    } else {
+      result.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+
+  return result.concat(left.slice(leftIndex), right.slice(rightIndex))
+}
+
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90]
+const sortedArray = mergeSort(unsortedArray)
+console.log(sortedArray) // Output: [11, 12, 22, 25, 34, 64, 90]
