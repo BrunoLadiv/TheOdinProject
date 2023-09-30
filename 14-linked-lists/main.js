e62e8ca271a38b275f1052b1baa3e0081c96178c
Bruno Vidal <brunokasprik@gmail.com>
2023-09-30 02:22:06 -0300

    binary search 1st commit

--- 14-linked-lists/main.js
+++ 14-linked-lists/main.js
@@ -2,6 +2,7 @@ const LinkedList = require('./LinkedList')
 
 // Example usage:
 const linkedList = new LinkedList()
+const linkedList2 = new LinkedList()
 linkedList.append(1)
 linkedList.append(2)
 linkedList.append(3)
@@ -19,3 +20,11 @@ linkedList.insertAt(4, 2)
 console.log(linkedList.toString()) // Output: (0) -> (1) -> (4) -> (2) -> null
 linkedList.removeAt(1)
 console.log(linkedList.toString()) // Output: (0) -> (4) -> (2) -> null
+
+linkedList2.append(10)
+linkedList2.append(20)
+linkedList2.append(30)
+linkedList2.prepend(0)
+
+console.log(linkedList2.toString(2)) // Output: (0) -> (10) -> (20) -> (30) -> null
+
