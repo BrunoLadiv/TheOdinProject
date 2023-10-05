fde38c6db8dc612cf6c3382e1fcd59a92cf2f2fc
Bruno Vidal <brunokasprik@gmail.com>
2023-10-04 23:54:43 -0300

    reverseString function test done

--- 17-testing-practice/index.js
+++ 17-testing-practice/index.js
@@ -2,8 +2,13 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
 
 export {
   capitalize,
-  
+  reverseString
+
 }
--- 17-testing-practice/index.test.js
+++ 17-testing-practice/index.test.js
@@ -1,8 +1,15 @@
-import { capitalize } from './index'
+import { capitalize, reverseString } from './index'
 
 describe('capitalize function', () => {
   it('capitalizes the first character of a string', () => {
     expect(capitalize('hello')).toBe('Hello')
     expect(capitalize('world')).toBe('World')
+  })
+})
+
+describe('reverseString function', () => {
+  it('reverses a string', () => {
+    expect(reverseString('hello')).toBe('olleh')
+    expect(reverseString('world')).toBe('dlrow')
   })
 })
