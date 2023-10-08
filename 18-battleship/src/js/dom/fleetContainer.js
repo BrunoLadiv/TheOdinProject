bc6d3e758c153dd2f825caffce1101cd531a6a0c
Bruno Vidal <brunokasprik@gmail.com>
2023-10-08 06:05:37 -0300

    added google fonts and bg

--- 18-battleship/src/js/dom/fleetContainer.js
+++ 18-battleship/src/js/dom/fleetContainer.js
@@ -8,7 +8,6 @@ function createFleetContainer() {
     const div = document.createElement('div')
     div.classList.add(ship.name)
     div.classList.add('ship')
-    div.innerText = ship.name
     div.setAttribute('data-shipname', ship.name)
     div.setAttribute('title', ship.name)
     div.setAttribute('ship-length', ship.length)
