cfb5b8738dd58683a340d5a35105d68e6275c38e
Bruno Vidal <brunokasprik@gmail.com>
2023-12-06 23:31:57 -0300

    memory game react, 1st commit

---
+++ 21-memory-card-game/src/App.jsx
@@ -0,0 +1,11 @@
+import GlobalStyles from "./styles/GlobalStyles"
+function App() {
+
+  return (
+    <>
+    <GlobalStyles/>
+    </>
+  )
+}
+
+export default App
---
+++ 21-memory-card-game/src/styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle} from 'styled-components'
+
+
+
+const GlobalStyles = createGlobalStyle`
+  html,body{
+    height: 100%;  
+    background-color: black;
+  }
+
+`
+export default GlobalStyles 
