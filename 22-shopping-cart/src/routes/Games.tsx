a9bd352d645ce2135730efce56d6d22a3d2e223d
Bruno <brunokasprik@gmail.com>
2023-12-27 23:22:00 -0300

    styled: all games page css

--- 22-shopping-cart/src/routes/Games.tsx
+++ 22-shopping-cart/src/routes/Games.tsx
@@ -1,29 +1,22 @@
-import styled from "styled-components"
+import styled from 'styled-components'
+import Card from '../components/Card'
 
 const AllGamesGridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   grid-gap: 2rem;
   min-height: 100vh;
+  justify-items: center;
 `
 export default function Games() {
   return (
-    <AllGamesGridContainer>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </AllGamesGridContainer>
+    <>
+      <h1>All games</h1>
+      <AllGamesGridContainer>
+      {[...Array(19)].map(index => (
+        <Card key={index} />
+      ))}
+      </AllGamesGridContainer>
+    </>
   )
 }
