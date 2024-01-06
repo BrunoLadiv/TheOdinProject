af079bb08b7da70c70fa75679e7cc7421a6d3a1a
Bruno <brunokasprik@gmail.com>
2024-01-06 18:38:36 -0300

    feature: added top250 api resquest

--- 22-shopping-cart/src/components/Carousel.tsx
+++ 22-shopping-cart/src/components/Carousel.tsx
@@ -17,23 +17,25 @@ const CarouselContainer = styled.section`
 const CarouselCategory = styled.h2`
   font-size: 1.2rem;
   font-weight: 100;
+
 `
-export default function Carousel(){
+interface Game {
+  name: string,
+  slug: string,
+  background_image: string
+}
+type CarouselProps = {
+  data?: Game[]
+}
+export default function Carousel({ data }: CarouselProps) {
   return (
     <>
-      <CarouselCategory>
-        Most Popular
-      </CarouselCategory>
+      <CarouselCategory>Most Popular</CarouselCategory>
       <CarouselContainer>
         <div>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {data?.map(game => {
+           return <Card game={game} />
+         })}
         </div>
       </CarouselContainer>
     </>
--- 22-shopping-cart/src/routes/Home.tsx
+++ 22-shopping-cart/src/routes/Home.tsx
@@ -1,13 +1,32 @@
 import Carousel from '../components/Carousel'
 import HeroSection from '../components/HeroSection'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const fetchTop250 = () =>
+  axios.get(
+    'https://rawg.io/api/games/lists/popular?discover=true&&page_size=20&page=1&key=c542e67aec3a4340908f9de9e86038af'
+  )
+const fetchNewReleases = () =>
+  axios.get(
+    'https://rawg.io/api/games/lists/recent-games-past?discover=true&ordering=-added&page_size=20&page=1&key=c542e67aec3a4340908f9de9e86038af'
+  )
+const fetchUpcoming = () =>
+  axios.get(
+    'https://rawg.io/api/games/calendar/platforms?key=c542e67aec3a4340908f9de9e86038af'
+  )
 
 function Home() {
+  const { data: top250Data } = useQuery('top-250', fetchTop250)
+  const { data: newReleasesData } = useQuery('new-releases', fetchTop250)
+  const { data: upcomingData } = useQuery('upcoming', fetchTop250)
+  console.log(top250Data?.data.results)
   return (
     <>
       <HeroSection />
-      <Carousel />
-      <Carousel />
-      <Carousel />
+      <Carousel data={top250Data?.data.results}/>
+      {/* <Carousel data={newReleasesData} />
+      <Carousel data={upcomingData}/> */}
     </>
   )
 }
