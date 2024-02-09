ff68fed145185a6aee736b817761f15195870b32
Bruno <brunokasprik@gmail.com>
2024-02-09 00:38:10 -0300

    add express-validator

--- 25-mdn-library/app.js
+++ 25-mdn-library/app.js
@@ -14,6 +14,7 @@ async function main() {
 
 main().catch((err) => console.log(err))
 
+const catalogRouter = require("./routes/catalog")
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
+app.use("/catalog", catalogRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
