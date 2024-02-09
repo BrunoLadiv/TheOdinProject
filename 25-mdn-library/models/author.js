fcf0e57ef19e6978a88dadc6264c67f7cde67f76
Bruno <brunokasprik@gmail.com>
2024-02-09 04:18:17 -0300

    updated author mod added remaining views

--- 25-mdn-library/models/author.js
+++ 25-mdn-library/models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const { DateTime } = require("luxon") // for date handling
 
 const Schema = mongoose.Schema
 
@@ -9,23 +10,39 @@ const AuthorSchema = new Schema({
   date_of_death: { type: Date },
 })
 
-// Virtual for author's full name
+// Virtual for author "full" name.
 AuthorSchema.virtual("name").get(function () {
-  // To avoid errors in cases where an author does not have either a family name or first name
-  // We want to make sure we handle the exception by returning an empty string for that case
-  let fullname = ""
-  if (this.first_name && this.family_name) {
-    fullname = `${this.family_name}, ${this.first_name}`
-  }
-
-  return fullname
+  return this.family_name + ", " + this.first_name
 })
 
-// Virtual for author's URL
+// Virtual for this author instance URL.
 AuthorSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/catalog/author/${this._id}`
+  return "/catalog/author/" + this._id
 })
 
-// Export model
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = ""
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    )
+  }
+  lifetime_string += " - "
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    )
+  }
+  return lifetime_string
+})
+
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate() // format 'YYYY-MM-DD'
+})
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate() // format 'YYYY-MM-DD'
+})
+
+// Export model.
 module.exports = mongoose.model("Author", AuthorSchema)
