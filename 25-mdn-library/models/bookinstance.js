d66a6b1ee379c0a137b7a3d4083a25bc9fa96af4
Bruno <brunokasprik@gmail.com>
2024-02-09 03:47:17 -0300

    add genre page and controller

--- 25-mdn-library/models/bookinstance.js
+++ 25-mdn-library/models/bookinstance.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
+const { DateTime } = require("luxon") //for date handling
 
 const Schema = mongoose.Schema
 
 const BookInstanceSchema = new Schema({
-  book: { type: Schema.Types.ObjectId, ref: "Book", required: true }, // reference to the associated book
+  book: { type: Schema.ObjectId, ref: "Book", required: true }, // Reference to the associated book.
   imprint: { type: String, required: true },
   status: {
     type: String,
@@ -14,11 +15,18 @@ const BookInstanceSchema = new Schema({
   due_back: { type: Date, default: Date.now },
 })
 
-// Virtual for bookinstance's URL
+// Virtual for this bookinstance object's URL.
 BookInstanceSchema.virtual("url").get(function () {
-  // We don't use an arrow function as we'll need the this object
-  return `/catalog/bookinstance/${this._id}`
+  return "/catalog/bookinstance/" + this._id
 })
 
-// Export model
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED)
+})
+
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.due_back).toISODate() //format 'YYYY-MM-DD'
+})
+
+// Export model.
 module.exports = mongoose.model("BookInstance", BookInstanceSchema)
