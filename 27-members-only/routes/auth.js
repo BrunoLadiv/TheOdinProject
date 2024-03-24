6438cf96b4c5ea19fd19e5b49f481b1abe5739e5
Bruno <brunokasprik@gmail.com>
2024-03-24 07:35:55 -0300

    directory change for backend2

--- 27-members-only/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createUser } from '../controllers/authController.js'
-
-const router = express.Router()
-
-router.route('/login').post()
-router.route('/register').post(createUser)
-
-export default router
