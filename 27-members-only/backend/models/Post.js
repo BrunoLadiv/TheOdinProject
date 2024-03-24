ba3d5b619f305e580204d6a7614f8e153417c97a
Bruno <brunokasprik@gmail.com>
2024-03-24 08:08:55 -0300

    add post schema

---
+++ 27-members-only/backend/models/Post.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    minlength: 1,
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 280,
+    minlength: 10,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export default mongoose.model('Post', postSchema)
--- 27-members-only/backend/models/User.js
+++ 27-members-only/backend/models/User.js
@@ -5,21 +5,41 @@ const userSchema = mongoose.Schema(
     name: {
       type: String,
       required: true,
+      minlength: 3,
+      maxlength: 50,
+      trim: true,
     },
     lastName: {
       type: String,
       required: true,
+      minlength: 3,
+      maxlength: 50,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: [true, 'Email already exists, Please try a different email!'],
+      maxlength: 100,
     },
     password: {
       type: String,
       required: true,
+      minlength: [
+        5,
+        'Password must be at least 6 characters long, Please try a different password!',
+      ],
+      maxlength: [
+        100,
+        'Password must be at most 100 characters long, Please try a different password!',
+      ],
+      select: false,
     },
     isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isMember: {
       type: Boolean,
       default: false,
     },
