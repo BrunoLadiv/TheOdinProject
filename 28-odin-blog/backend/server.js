00c92335702977f028b44506a5acdbec5633e379
Bruno <brunokasprik@gmail.com>
2024-04-07 20:51:40 -0300

    1st commit, server config

---
+++ 28-odin-blog/backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err))
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
