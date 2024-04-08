adb8e1ccb29dc975e5b85e588d38f68c47665441
Bruno <brunokasprik@gmail.com>
2024-04-07 22:03:12 -0300

    add post model

---
+++ 28-odin-blog/backend/models/Post.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const Post = new mongoose.SchemaType({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+})
+
+export default mongoose.model('Post', Post)
--- 28-odin-blog/backend/server.js
+++ 28-odin-blog/backend/server.js
@@ -16,6 +16,8 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
