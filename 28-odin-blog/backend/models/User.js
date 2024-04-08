af31bd961695518cb118b6573cd2bb6cc5e6d50a
Bruno <brunokasprik@gmail.com>
2024-04-07 22:28:12 -0300

    add user model

---
+++ 28-odin-blog/backend/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const User = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+export default mongoose.model('User', User)
