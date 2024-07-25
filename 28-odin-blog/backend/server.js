c4c4ce62f0c33620fe3778988526a822876c1580
Bruno Vidal <brunokasprik@gmail.com>
2024-07-25 06:14:04 -0300

    add cors to server

--- 28-odin-blog/backend/server.js
+++ 28-odin-blog/backend/server.js
@@ -1,32 +1,47 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import authRouter from './routes/authRouter.js'
-import postsRouter from './routes/postsRouter.js'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRouter from "./routes/authRouter.js";
+import postsRouter from "./routes/postsRouter.js";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log(err))
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
+const whitelist = ["http://localhost:3000", "http://127.0.0.1:5173"];
 
-app.use(express.json())
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
 
-app.use('/api/auth', authRouter)
-app.use('/api/posts', postsRouter)
+const app = express();
 
-app.get('*', (req, res) => {
-  res.status(404).json({ error: 'Not found' })
-})
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postsRouter);
+
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
