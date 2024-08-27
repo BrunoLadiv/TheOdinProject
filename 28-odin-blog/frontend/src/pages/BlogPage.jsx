163e89728f79091123afa97a52dd87871a974dbf
Bruno Vidal <brunokasprik@gmail.com>
2024-08-26 23:54:40 -0300

    add blog page, blog working

--- 28-odin-blog/frontend/src/pages/BlogPage.jsx
+++ 28-odin-blog/frontend/src/pages/BlogPage.jsx
@@ -1,6 +1,16 @@
 import { useParams } from "react-router-dom";
+import { useGetPostQuery } from "../features/posts/postApiSlice.js";
 
 export default function BlogPage() {
   const { blogId } = useParams();
-  return <div>blog id is {blogId}</div>;
+  const { data, isLoading, error } = useGetPostQuery({ id: blogId });
+  console.log(data);
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error</h1>;
+  return (
+    <div>
+      <h1>{data.post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.post.content }} />
+    </div>
+  );
 }
