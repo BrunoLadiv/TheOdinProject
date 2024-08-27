1076fbe7cf7e31ed7812ca07710803d3071cb090
Bruno Vidal <brunokasprik@gmail.com>
2024-08-26 23:16:19 -0300

    add single post route/controller

--- 28-odin-blog/backend/controllers/postsController.js
+++ 28-odin-blog/backend/controllers/postsController.js
@@ -1,69 +1,80 @@
-import Post from '../models/Post.js'
+import Post from "../models/Post.js";
 
 const createPost = async (req, res) => {
-  const { id: author } = req.user
-  const { title, content } = req.body
+  const { id: author } = req.user;
+  const { title, content } = req.body;
   const newPost = new Post({
     title,
     content,
     author,
-  })
+  });
 
   try {
-    const savedPost = await newPost.save()
+    const savedPost = await newPost.save();
     res
       .status(201)
-      .json({ post: savedPost, message: 'Post created successfully' })
+      .json({ post: savedPost, message: "Post created successfully" });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
+};
+
+const getPost = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const postId = req.params.id;
+    const post = await Post.findById(postId);
+    res.status(200).json({ post });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const getPosts = async (req, res) => {
-  console.log(req.query)
+  console.log(req.query);
   try {
-    const page = parseInt(req.query.page) || 1
-    const limit = parseInt(req.query.limit) || undefined
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || undefined;
 
     // Calculate skip based on page and limit
-    const skip = (page - 1) * limit
+    const skip = (page - 1) * limit;
 
     // Fetch posts for the current page
-    const posts = await Post.find().sort({ date: -1 }).skip(skip).limit(limit)
+    const posts = await Post.find().sort({ date: -1 }).skip(skip).limit(limit);
 
     // Fetch total count of posts (for pagination)
-    const totalCount = await Post.countDocuments()
+    const totalCount = await Post.countDocuments();
 
     // Calculate total pages
-    const totalPages = Math.ceil(totalCount / limit)
+    const totalPages = Math.ceil(totalCount / limit);
 
     // Response with pagination information
-    res.status(200).json({ posts, totalPages, currentPage: page })
+    res.status(200).json({ posts, totalPages, currentPage: page });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 const getUserPosts = async (req, res) => {
-  const { id: author } = req.user
+  const { id: author } = req.user;
   try {
-    const posts = await Post.find({ author })
-    res.status(200).json(posts)
+    const posts = await Post.find({ author });
+    res.status(200).json(posts);
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 const deletePost = async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   try {
-    const post = await Post.findByIdAndDelete(id)
+    const post = await Post.findByIdAndDelete(id);
     if (!post) {
-      return res.status(404).json({ message: 'Post not found' })
+      return res.status(404).json({ message: "Post not found" });
     }
-    res.status(200).json({ message: 'Post deleted successfully' })
+    res.status(200).json({ message: "Post deleted successfully" });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
-export { createPost, getUserPosts, getPosts, deletePost }
+};
+export { getPost, createPost, getUserPosts, getPosts, deletePost };
--- 28-odin-blog/backend/routes/postsRouter.js
+++ 28-odin-blog/backend/routes/postsRouter.js
@@ -1,14 +1,15 @@
-import express from 'express'
+import express from "express";
 import {
+  getPost,
   createPost,
   getPosts,
   deletePost,
-} from '../controllers/postsController.js'
-import checkToken from '../utilities/checkToken.js'
+} from "../controllers/postsController.js";
+import checkToken from "../utilities/checkToken.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').get(getPosts).post(checkToken, createPost)
-router.route('/:id').delete(checkToken, deletePost)
+router.route("/:id").get(getPost).delete(checkToken, deletePost);
+router.route("/").get(getPosts).post(checkToken, createPost);
 
-export default router
+export default router;
