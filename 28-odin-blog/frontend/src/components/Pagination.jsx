8f38a1866f0aab9fb4f632dee921e9883814b7f3
Bruno Vidal <brunokasprik@gmail.com>
2024-09-21 21:51:38 -0300

    add pagination

--- 28-odin-blog/frontend/src/components/Pagination.jsx
+++ 28-odin-blog/frontend/src/components/Pagination.jsx
@@ -1,44 +1,47 @@
-const Pagination = () => {
+const Pagination = ({ handlePageChange, currentPage, totalPages }) => {
   return (
-    <div class="flex items-center gap-8">
+    <div className="flex prose dark:prose-invert items-center gap-8">
       <button
-        disabled
-        class="rounded-md border border-slate-300 p-2.5 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        disabled={currentPage === 1}
+        className="rounded-md border border-slate-300 p-2.5 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
+        onClick={() => handlePageChange(currentPage - 1)}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="w-4 h-4"
+          className="w-4 h-4"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           />
         </svg>
       </button>
 
-      <p class="text-slate-600">
-        Page <strong class="text-slate-800">1</strong> of&nbsp;
-        <strong class="text-slate-800">10</strong>
+      <p>
+        Page <strong>{currentPage}</strong> of&nbsp;
+        <strong>{totalPages}</strong>
       </p>
 
       <button
-        class="rounded-md border border-slate-300 p-2.5 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        className="rounded-md border border-slate-300 p-2.5 text-center text-sm transition-all shadow-sm hover:shadow-lg hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
+        disabled={currentPage === totalPages}
+        onClick={() => handlePageChange(currentPage + 1)}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          class="w-4 h-4"
+          className="w-4 h-4"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           />
         </svg>
       </button>
--- 28-odin-blog/frontend/src/pages/Home.jsx
+++ 28-odin-blog/frontend/src/pages/Home.jsx
@@ -1,23 +1,37 @@
-import { useGetPostsQuery } from '../features/posts/postApiSlice'
-import { useNavigate } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
-import BlogCard from '../components/BlogCard'
+import { useGetPostsQuery } from "../features/posts/postApiSlice";
+import { useSearchParams, useLocation, useNavigate } from "react-router-dom";
+
+import { useParams } from "react-router-dom";
+import BlogCard from "../components/BlogCard";
+import Pagination from "../components/Pagination";
 
 const HomePage = () => {
-  const { tag } = useParams()
-  const { data, error, isLoading } = useGetPostsQuery({ page: 1, limit: 10, tag })
-  const navigate = useNavigate()
+  const { tag } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(location);
+  console.log(searchParams);
+  const currentPage = parseInt(searchParams.get("page")) || 1;
+  const { data, error, isLoading } = useGetPostsQuery({
+    page: currentPage,
+    limit: 10,
+    tag,
+  });
+  const handlePageChange = (newPage) => {
+    // Update the query param for page
+    setSearchParams({ page: newPage });
+  };
 
-  console.log(tag)
-  if (isLoading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-  console.log(data)
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log(data);
   const goToPost = (id) => {
-    navigate(`/blog/${id}`)
-  }
+    navigate(`/blog/${id}`);
+  };
   const goToTag = (tag) => {
-    navigate(`/tags/${tag}`)
-  }
+    navigate(`/tags/${tag}`);
+  };
   return (
     <div className="min-w-full flex-grow">
       <h1>Posts</h1>
@@ -31,8 +45,13 @@ const HomePage = () => {
           />
         ))}
       </ul>
+      <Pagination
+        handlePageChange={handlePageChange}
+        currentPage={data.currentPage}
+        totalPages={data.totalPages}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
