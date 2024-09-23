f52fd26ab52f16cc09e1a9759b1f92d09c5ef472
Bruno Vidal <brunokasprik@gmail.com>
2024-09-23 06:07:58 -0300

    add css for the mobile menu

--- 28-odin-blog/frontend/src/components/Nav.jsx
+++ 28-odin-blog/frontend/src/components/Nav.jsx
@@ -1,43 +1,46 @@
-import { RxHamburgerMenu } from 'react-icons/rx'
-import { IoMdClose } from 'react-icons/io'
-import { useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-function Links() {
-  const location = useLocation()
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+function Links({ setIsOpen }) {
+  const location = useLocation();
 
   return (
     <>
       <Link
-        className={`hover:text-[#ed8796] ${
-          location.pathname === '/' ? 'border-b border-blue-500' : ''
+        onClick={() => setIsOpen(false)}
+        className={`mt-16 hover:text-[#ed8796] ${
+          location.pathname === "/" ? "border-b border-blue-500" : ""
         }`}
         to="/"
       >
         Blog
       </Link>
       <Link
+        onClick={() => setIsOpen(false)}
         className={`hover:text-[#ed8796] ${
-          location.pathname.startsWith('/tags')
-            ? 'border-b border-blue-500'
-            : ''
+          location.pathname.startsWith("/tags")
+            ? "border-b border-blue-500"
+            : ""
         }`}
-        to={'/tags'}
+        to={"/tags"}
       >
         Tags
       </Link>
       <Link
+        onClick={() => setIsOpen(false)}
         className={`hover:text-[#ed8796] ${
-          location.pathname.startsWith('/projects')
-            ? 'border-b border-blue-500'
-            : ''
+          location.pathname.startsWith("/projects")
+            ? "border-b border-blue-500"
+            : ""
         }`}
         to="/projects"
       >
         Projects
       </Link>
     </>
-  )
+  );
 }
 function NavLinks({ isOpen, setIsOpen }) {
   return (
@@ -59,21 +62,25 @@ function NavLinks({ isOpen, setIsOpen }) {
         )}
       </div>
     </>
-  )
+  );
 }
 
 export default function Nav() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav>
       <NavLinks isOpen={isOpen} setIsOpen={setIsOpen} />
       {isOpen && (
-        <div className="md:hidden flex justify-center fixed inset-0 bg-black/50 z-10">
-          <div className="flex flex-col gap-2 mt-16 text-xl">
-            <Links />
+        <div className="md:hidden flex justify-end fixed inset-0 bg-black/50 z-10">
+          <div
+            className={`pl-8 ${
+              isOpen ? "translate-x-0" : "translate-x-full"
+            }  dark:bg-black bg-white min-w-[70%] border-black flex flex-col gap-2 text-xl`}
+          >
+            <Links setIsOpen={setIsOpen} />
           </div>
         </div>
       )}
     </nav>
-  )
+  );
 }
