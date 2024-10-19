78e4f2a42d0f9dcb07ee5a1b56f9e72f0d203d8c
Bruno Vidal <brunokasprik@gmail.com>
2024-10-18 23:11:49 -0300

    add delete and edit icon only show if user is admin

--- 28-odin-blog/frontend/src/pages/BlogPage.jsx
+++ 28-odin-blog/frontend/src/pages/BlogPage.jsx
@@ -11,11 +11,13 @@ import Comment from "../components/Comment.jsx";
 import CommentForm from "../components/CommentForm.jsx";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import { useSelector } from "react-redux";
 
 export default function BlogPage() {
   const { slug } = useParams();
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
 
   const [
     createComment,
@@ -83,17 +85,19 @@ export default function BlogPage() {
       </div>
       <div className="prose dark:prose-invert relative md:col-span-3 col-span-4">
         <h1>{data.post.title}</h1>
+        {token && (
+          <div className="absolute top-0 right-5 flex gap-4">
+            <Link to="/edit-blog" state={{ blog: data.post }}>
+              <FaEdit className="hover:scale-125" title="Edit Blog" />
+            </Link>
+            <MdDelete
+              className="hover:scale-125 hover:cursor-pointer"
+              title="Delete Blog"
+              onClick={handleDelete}
+            />
+          </div>
+        )}
 
-        <div className="absolute top-0 right-5 flex gap-4">
-          <Link to="/edit-blog" state={{ blog: data.post }}>
-            <FaEdit className="hover:scale-125" title="Edit Blog" />
-          </Link>
-          <MdDelete
-            className="hover:scale-125 hover:cursor-pointer"
-            title="Delete Blog"
-            onClick={handleDelete}
-          />
-        </div>
         <div dangerouslySetInnerHTML={{ __html: data.post.content }} />
       </div>
       <h2 className="md:col-start-2 text-xl">Comments </h2>
