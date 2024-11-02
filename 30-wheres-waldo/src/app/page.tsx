35bd252c117b7e84ddb27a57bd665ea442a4addb
Bruno Vidal <brunokasprik@gmail.com>
2024-11-02 18:54:42 -0300

    add feature to track the image x and y position, consistent in all screen sizes

--- 30-wheres-waldo/src/app/page.tsx
+++ 30-wheres-waldo/src/app/page.tsx
@@ -1,18 +1,42 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Home() {
-  const [cords, setCords] = useState({ x: 0, y: 0 });
+  const [imgCoords, setImgCoords] = useState({ xPercent: 0, yPercent: 0 });
+  const imgRef = useRef(null);
+  const [showDD, setShowDD] = useState(false);
 
-  console.log(cords);
-  function handleMove(e) {
-    setCords({ x: e.clientX, y: e.clientY });
+  function handleImageClick(event) {
+    setShowDD(!showDD);
+  }
+
+  function handleMouseMove(event) {
+    if (imgRef.current) {
+      const img = imgRef.current;
+      const rect = img.getBoundingClientRect();
+
+      const xOffset = event.pageX - rect.left - window.scrollX;
+      const yOffset = event.pageY - rect.top - window.scrollY;
+
+      const xPercent = (xOffset / rect.width) * 100;
+      const yPercent = (yOffset / rect.height) * 100;
+
+      setImgCoords({ xPercent, yPercent });
+      console.log(
+        `Mouse moved to: X=${xPercent.toFixed(2)}%, Y=${yPercent.toFixed(2)}%`,
+      );
+    }
   }
 
   return (
-    <div onMouseMove={handleMove} className="w-screen overflow-x-auto">
+    <div
+      onMouseMove={handleMouseMove}
+      className="relative overflow-x-auto min-w-screen"
+    >
       <img
-        className="min-w-[1024px]"
+        ref={imgRef}
+        onClick={handleImageClick}
+        className="min-w-[1024px] max-w-none"
         src="/images/game1.jpeg"
         alt="find game img"
       />
